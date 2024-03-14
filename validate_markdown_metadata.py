04784a3ea484172e0849d81466129c168a3cd299
Jose Ayerdis <joseayerdis@gmail.com>
2024-03-13 23:35:46 -0700

    Front matter version and script

--- validate_markdown_metadata.py
+++ validate_markdown_metadata.py
@@ -1,11 +1,11 @@
 from sys import argv
 
-import frontmatter
+from  frontmatter import Frontmatter
 
 KEYS = ['title', 'datatype', 'sources', 'destinations']
 
 def article_validator(markdown_file):
-  article = frontmatter.load(markdown_file)
+  article = Frontmatter.read_file(markdown_file)
   keys_indices = {key: index for index, key in enumerate(KEYS)}
   sorted_keys = sorted(list(article.keys()), key=lambda x: keys_indices.get(x, float('inf')))
 
